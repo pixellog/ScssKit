e4b1c15d359f8c80e1f8d5c35964c7a362d99b99
pixellog <pixellog@gmail.com>
2017-05-25 05:00:59 +0900

    dashboard comp

--- js/common.js
+++ js/common.js
@@ -139,11 +139,11 @@ $(function () {
             $.each(data, function (i) {
                 items.push('<li><img src="' + this.icon + '" alt=""><span class="title">' + this.title + '</span><span class="value">' + this.value + '</span></li>');
             });
-            $target.find('.result').html(items.join(''));
+            $target.append('<ul class="legend">' + items.join('') + '</ul>');
         },
         deviceConditionSet: function () { // 단말기상태
             var $target = $('.device-condition');
-            $target.append('<canvas id="deviceGraph" width="160" height="160"></canvas>');
+            $target.append('<canvas id="deviceGraph" width="210" height="210"></canvas>');
 
             var drawChart = function () {
                 var data = [
@@ -156,73 +156,83 @@ $(function () {
                 // 범례그리기
                 var items = [];
                 var total = 0;
-                items.push('<ul class="legend" style="left: 220px; top: 100px;">');
+                items.push('<ul class="legend">');
                 $.each(data, function () {
                     items.push('<li><i style="background-color:' + this.color + '" /><span class="title">' + this.label + '</span><span class="value" style="color:' + this.color + '">' + this.value + '</span></li>');
                     total += this.value;
                 });
                 items.push('</ul>');
-                items.push('<div class="counter"><img src="/images/common/check_ty1.png" alt="" /><strong>' + total + '</strong>대</div>');
-                $target.append(items.join(''));
+                $target.append(items.join('') + '<div class="counter"><img src="/img/ico_device.png" alt="" /><strong>' + total + '</strong>대</div>');
             }();
         },
         errorConditionSet: function () { // 장애현황
             var $target = $('.error-condition');
 
-            var data = {
-                '기능불량': 0,
-                '통신불량': 0,
-                '배터리불량': 0,
-                '파손': 1,
-                '분실': 1,
-                '기타': 0
-            };
+            var data = [
+                {title: '기능불량', value: 0, icon: '/img/ico_function.png'},
+                {title: '통신불량', value: 0, icon: '/img/ico_communi.png'},
+                {title: '배터리불량', value: 0, icon: '/img/ico_battery.png'},
+                {title: '파손', value: 1, icon: '/img/ico_broken.png'},
+                {title: '분실', value: 1, icon: '/img/ico_lost.png'},
+                {title: '기타', value: 0, icon: '/img/ico_etc.png'}
+            ];
             var items = [];
             var total = 0;
-            $.each(data, function (key, val) {
-                items.push('<li><span class="title">' + key + '</span><span class="value">' + val + '</span></li>');
-                total += val;
+            $.each(data, function (i) {
+                items.push('<li><span class="icon"><img src="' + this.icon + '" alt=""></span><span class="title">' + this.title + '</span><span class="value">' + this.value + '</span></li>');
+                total += this.value;
                 return total;
             });
-            $target.find('.result').html('<div class="total">' + total + '건</div><ul>' + items.join('') + '</ul>');
+            $target.append('<div class="counter"><img src="/img/ico_warning.png" alt="" /> <strong>' + total + '</strong> 건</div><ul class="legend">' + items.join('') + '</ul>');
         }
         ,
         timeConditionSet: function () { // 시간대별 수신현황
             var $target = $('.time-condition');
-            $target.append('<canvas id="timeGraph" width="680" height="180"></canvas>');
+            $target.append('<canvas id="timeGraph" width="980" height="190"></canvas>');
 
-            // chart
-            var data = {
-                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
-                datasets: [
-                    {
-                        label: "정상",
-                        fillColor: "rgba(50,229,50,0.2)",
-                        strokeColor: "#9BC53D",
-                        pointColor: "#02C39A",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: [100, 100, 100, 100, 90, 90, 80, 80, 70, 70, 65, 59, 59, 71, 56, 65, 59, 71, 56, 65, 80, 90, 90, 100]
-                    },
-                    {
-                        label: "지연",
-                        fillColor: "rgba(255,50,50,0.2)",
-                        strokeColor: "#E55934",
-                        pointColor: "#E55934",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: [0, 0, 0, 0, 10, 10, 20, 20, 30, 40, 12, 16, 20, 15, 34, 43, 70, 60, 50, 50, 45, 32, 10, 34]
-                    }
-                ]
-            };
-            var context = document.getElementById("timeGraph").getContext("2d");
-            new Chart(context).Line(data);
+            var drawChart = function () {
+
+                var data = {
+                    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+                    datasets: [
+                        {
+                            label: "정상",
+                            fillColor: "rgba(50,229,50,0.2)",
+                            strokeColor: "#9BC53D",
+                            pointColor: "#02C39A",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(220,220,220,1)",
+                            data: [100, 100, 100, 100, 90, 90, 80, 80, 70, 70, 65, 59, 59, 71, 56, 65, 59, 71, 56, 65, 80, 90, 90, 100]
+                        },
+                        {
+                            label: "지연",
+                            fillColor: "rgba(255,50,50,0.2)",
+                            strokeColor: "#E55934",
+                            pointColor: "#E55934",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(151,187,205,1)",
+                            data: [0, 0, 0, 0, 10, 10, 20, 20, 30, 40, 12, 16, 20, 15, 34, 43, 70, 60, 50, 50, 45, 32, 10, 34]
+                        }
+                    ]
+                };
+                var context = document.getElementById("timeGraph").getContext("2d");
+                new Chart(context).Line(data);
+
+                // 범례그리기
+                var items = [];
+                items.push('<ul class="legend">');
+                $.each(data.datasets, function () {
+                    items.push('<li><i style="background-color:' + this.strokeColor + '" /><span class="title">' + this.label + '</span></li>');
+                });
+                items.push('</ul>');
+                $target.append(items.join(''));
+            }();
         },
         geoFencingSet: function () {
             var $target = $('.geo-fencing');
-            $target.append('<canvas id="geoFencing" width="150" height="120" style="margin:40px 0 0 -10px"></canvas>');
+            $target.append('<canvas id="geoFencing" width="152" height="152"></canvas>');
 
             var drawChart = function () {
                 var data = [
@@ -246,7 +256,7 @@ $(function () {
         },
         eventConditionSet: function () {
             var $target = $('.event-condition');
-            $target.append('<canvas id="eventCondition" width="150" height="120" style="margin:40px 0 0 -10px"></canvas>');
+            $target.append('<canvas id="eventCondition" width="152" height="152"></canvas>');
 
             var drawChart = function () {
                 var data = [
