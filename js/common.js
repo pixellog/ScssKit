ed0a8eb56c236367f5b02f5e78c5adb465e41531
loh <pixellog@gmail.com>
2017-06-02 18:03:58 +0900

    highchart

--- js/common.js
+++ js/common.js
@@ -1,121 +1,168 @@
 $(function () {
 
-    /*jQuery.fn.extend({
-     toggleButton: function () {
-     this.each(function () {
-     var $this = $(this);
-     $this.find('button').on({
-     click: function () {
-     $(this).addClass('on').siblings().removeClass('on');
-     }
-     });
-     })
-     }
-     });*/
+    var ui = {
+        init: function () {
+            $('.tabs').tabs(); // jquery ui tabs
+            this.sidebar(); // sidebar toggle
+            this.circleGraph(); // 로그인 영역 하단 서클 그래프
+        },
+        sidebar: function () {
+            $('.btn-sidebar').on({
+                click: function () {
+                    $(this).toggleClass('active');
+                    var check = $(this).hasClass('active');
+                    $('#body').animate({left: check ? -360 : 0}, 200);
+                }
+            });
+        },
+        circleGraph: function () {
+            var $target = $('.map-area-graph');
 
-    $('.tabs').tabs();
+            $target.append('<div id="graph1"></div>');
+            $target.append('<div id="graph2"></div>');
+            $target.append('<div id="graph3"></div>');
 
-    // sidebar slide button
-    $('.btn-slide-toggle').on({
-        click: function () {
-            $(this).toggleClass('active');
-            var check = $(this).hasClass('active');
-            $('#sidebar').animate({left: check ? -342 : 0}, 200);
-            $('.map-area-graph').animate({left: check ? 10 : 352}, 200);
-        }
-    });
+            var data = [
+                {label: '정상', value: 12, color: "#16db16"},
+                {label: '장애', value: 2, color: "#f57a00"},
+                {label: '배터리경고', value: 2, color: "#f02c0e"}
+            ];
 
-    // 뉴스티커
-    $('.news-ticker > div').bxSlider({
-        mode: 'vertical',
-        auto: true, // auto rolling
-        autoHover: true,
-        autoControls: false, // play,stop button display
-        pager: false,
-        slideWidth: 500
-    });
-
-    // 지도위 그래프
-    function mapAreaGraph() {
-        var $target = $('.map-area-graph');
-
-        var data = [
-            {value: 8, color: "#16db16", label: '정상'},
-            {value: 4, color: "#f57a00", label: '장애'}
-        ];
-
-        $target.append('<h4>전체 <strong>' + ~~(data[0].value + data[1].value) + '</strong></h4>');
-
-        $target.append('<div class="graph1"><canvas id="mapAreaGraph" width="82" height="82"></canvas></div>');
-        var drawChart = function () {
-            var data1 = $.extend(true, {}, data);
-            data1[1].color = 'rgba(0,0,0,0)';
-            var mapAreaGraph = new Chart(document.getElementById("mapAreaGraph").getContext("2d")).Doughnut(data1, {percentageInnerCutout: 65});
-
-            // 범례그리기
-            var items = [];
-            items.push('<ul class="legend"><li><strong class="title" style="color:' + data1[0].color + '">' + data[0].label + '</strong></li></ul>');
-            items.push('<div class="counter"><strong>' + data[0].value + '</strong></div>');
-            $target.find('.graph1').append(items.join(''));
-        }();
-
-        $target.append('<div class="graph2"><canvas id="mapAreaGraph2" width="82" height="82"></canvas></div>');
-        var drawChart2 = function () {
-            var data2 = $.extend(true, {}, data);
-            data2[0].color = 'rgba(0,0,0,0)';
-            // data[1].color = '#f57a00';
-            var mapAreaGraph2 = new Chart(document.getElementById("mapAreaGraph2").getContext("2d")).Doughnut(data2, {percentageInnerCutout: 65});
-
-            // 범례그리기
-            var items = [];
-            items.push('<ul class="legend"><li><strong class="title" style="color:' + data2[1].color + '">' + data[1].label + '</strong></li></ul>');
-            items.push('<div class="counter"><strong>' + data[1].value + '</strong></div>');
-            $target.find('.graph2').append(items.join(''));
-        }();
-    }
-
-    mapAreaGraph();
-
-
-    var x;
-    $('[data-url]').on({
-        click: function () {
-            var $this = $(this);
-            var url = $this.data('url');
-
-            if (x) return;
-
-            x = $.ajax({
-                url: url,
-                context: document.body
-            }).done(function (data) {
-                $(this).append(data);
-                return true;
+            Highcharts.setOptions({
+                title: false,
+                chart: {
+                    type: 'pie',
+                    marginRight: 0,
+                    spacing: [0, 0, 0, 0],
+                    margin:0
+                },
+                plotOptions: {
+                    pie: {
+                        showInLegend: true,
+                        dataLabels: false,
+                        startAngle: 0,
+                        endAngle: 360
+                    }
+                },
+                legend: {
+                    align: 'right',
+                    verticalAlign: 'top',
+                    floating: true,
+                    x: 10,
+                    y: 5,
+                    labelFormatter: function () {
+                        return '<span>' + this.name + '</span><br><span style="font-weight: normal;">' + this.series.total + '</span>';
+                    }
+                },
+                series: {
+                    groupPadding: 0
+                },
+                credits: {
+                    enabled: false
+                }
             });
 
+            /*var arr = [1,2,3];
+
+            $.each(arr, function (i) {
+                var chart1 = new Highcharts.Chart({
+                    chart: {
+                        renderTo: 'graph'+i
+                    },
+                    colors: [data[i].color],
+                    series: [{
+                        innerSize: '76%',
+                        data: [
+                            [data[i].label, data[i].value]
+                        ]
+                    }]
+                });
+            });*/
+
+
+            var chart1 = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'graph1'
+                },
+                colors: ['#16db16'],
+                series: [{
+                    innerSize: '76%',
+                    data: [
+                        ['정상', 12]
+                    ]
+                }]
+            });
+
+            var chart2 = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'graph2'
+                },
+                colors: ['#f57a00'],
+                series: [{
+                    innerSize: '76%',
+                    data: [
+                        ['장애', 2]
+                    ]
+                }]
+            });
+
+            var chart3 = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'graph3'
+                },
+                colors: ['#f02c0e'],
+                series: [{
+                    innerSize: '76%',
+                    data: [
+                        ['배터리경고', 2]
+                    ]
+                }],
+                legend: {
+                    x: 40
+                }
+            });
+
+            // 범례심볼 숨기기
+            $('.highcharts-legend-item .highcharts-point').hide();
+        }
+    }.init();
+
+
+    $.fn.extend({
+        ajaxPopupLoad: function () {
+            this.each(function () {
+                var $this = $(this);
+                var x;
+                var id;
+                $this.on({
+                    click: function () {
+                        var $this = $(this);
+                        var url = $this.data('url');
+
+                        if (id !== undefined) $('#' + id).dialog('open');
+
+                        if (x !== undefined) return;
+
+                        x = $.ajax({
+                            url: url,
+                            context: document.body
+                        }).done(function (data) {
+                            id = $(this).append(data).find('.popup').last().attr('id');
+                            return id;
+                        });
+
+                    }
+                });
+
+            });
         }
     });
 
-    // 탭추가
-    $('#btnTabAdd').on({
-        click: function () {
-            $("#insrstRgnMapAddPopup2").dialog('open');
-        }
-    });
 
-    // 자산추가
-    $('#btnAssetAdd').on({
-        click: function () {
-            $("#dialogAssetRegist").dialog('open');
-        }
-    });
+    $('[data-url]').ajaxPopupLoad();
 
-    // 대시보드
-    $('.btn-dashboard').on({
-        click: function () {
-            $("#dialogDashboard").dialog('open');
-        }
-    });
+    // 자산등록
+    //$('.btn-asset-regist').trigger('click');
 
 });
 
