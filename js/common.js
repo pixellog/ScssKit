eaea8e5a729b295baf1866f0af9f32b39c5399dd
loh <pixellog@gmail.com>
2017-07-14 13:52:20 +0900

    first add

--- js/common.js
+++ js/common.js
@@ -1,4 +1,16 @@
-/**
- * Created by loh on 2016-12-16.
- */
-console.log('oook')
+import $ from 'jquery';
+$(function () {
+    /*$.ajax({
+        dataType: 'json',
+        url: '/js/yayoung.json',
+        cache: false
+    }).done(function (data) {
+        var ids = data.fields[0].id;
+        console.log(JSON.stringify(data.records[0].야영(캠핑)장명));
+    });*/
+
+    $('.container').accordion({
+        active : 0,
+        collapsible : true
+    });
+});
